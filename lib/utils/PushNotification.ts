9a1b2365d8489daabc9e0d1a884b2518a540e5d4
Ramkrishna Maity <ramkrishnamaity4146@gmail.com>
2024-10-30 08:57:00 +0530

    Initial Commit

--- lib/utils/PushNotification.ts
+++ lib/utils/PushNotification.ts
@@ -1,31 +1,31 @@
-import admin from "firebase-admin";
-import UserModel from "../../models/User";
+// import admin from "firebase-admin";
+// import UserModel from "../../models/User";
 
-const serviceAccount = require("./firebase-sdk.json");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+// const serviceAccount = require("./firebase-sdk.json");
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
 
-export const sendNotification = async (receiver: string, message: string, sender: string) => {
-    try {
+// export const sendNotification = async (receiver: string, message: string, sender: string) => {
+//     try {
 
-        const receiverToken = await UserModel.findById(receiver);
-        if (receiverToken && receiverToken.deviceToken) {
-            const messageData = {
-                notification: {
-                    title: "Rhythmchat",
-                    body: "message",
-                    // icon: 'http://127.0.0.1:4050/api/v1/uploads/profile.png'
-                },
-                token: receiverToken.deviceToken
-            };
-            console.log("first", messageData);
+//         const receiverToken = await UserModel.findById(receiver);
+//         if (receiverToken && receiverToken.deviceToken) {
+//             const messageData = {
+//                 notification: {
+//                     title: "Rhythmchat",
+//                     body: "message",
+//                     // icon: 'http://127.0.0.1:4050/api/v1/uploads/profile.png'
+//                 },
+//                 token: receiverToken.deviceToken
+//             };
+//             console.log("first", messageData);
 
-            const response = await admin.messaging().send(messageData);
-            console.log(response, "responce of push notification");
-        }
+//             const response = await admin.messaging().send(messageData);
+//             console.log(response, "responce of push notification");
+//         }
 
-    } catch (error) {
-        console.log("error on send push notification", error);
-    }
-};
+//     } catch (error) {
+//         console.log("error on send push notification", error);
+//     }
+// };
